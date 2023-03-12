d5a50efee55c63af077e7f2483a08a8a480202b5
afsalsb <afsalsb@gmail.com>
2023-03-11 19:40:06 -0500

    git commit new1

--- src/JavaProject/HelloWorld.java
+++ src/JavaProject/HelloWorld.java
@@ -3,10 +3,8 @@ package JavaProject;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	System.out.println("Git Hub Project");
-	System.out.println("Git hub projct 2");
-	System.out.println("Git hub projct 3");
-
+	
+System.out.println("testing");
 	}
 
 }
---
+++ src/JavaProject/Secondclass.java
@@ -0,0 +1,11 @@
+package JavaProject;
+
+public class Secondclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is sednd clas");
+
+	}
+
+}
